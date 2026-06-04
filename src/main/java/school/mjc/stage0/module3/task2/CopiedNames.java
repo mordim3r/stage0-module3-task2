ccb0f920406060d0a35f7a2cfda45aeb36b82079
mordim3r <valent1n.bazylev@gmai.com>
2026-06-04 11:11:14 +0300

    fix dellete

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,17 +2,17 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        {
-        int variable = 0;}
-        {
-        int variable = 2000;
-        }
-        { int value = 1000;}
-        int variable = 4000;
+
+        int variable = 0;
+
+
+
+         int value = 1000;
+
         int value2 = 3000;
-        {int variables = 5000;}
-        { int variables = 7000;}
-        {int value = 90000;}
+        int variables = 5000;
+
+
 
 
     }
